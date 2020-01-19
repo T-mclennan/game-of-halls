db57e97d391347bfc8028781088c77849735d523
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-01-18 20:11:30 -0800

    initialized the project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h5>Game of Halls!!!</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+// Bodyparser Middleware
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+  });
+}
+
+server.listen(port, () => console.log(`Server started on port ${port}`));
