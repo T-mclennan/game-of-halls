4ff661502359958354502b4aa9e0fc0bf293580d
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-01-19 12:07:46 -0800

    added some styling and documentation

--- client/src/pages/Contributions.js
+++ client/src/pages/Contributions.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
+import { Container } from "reactstrap";
 
 export class Contributions extends Component {
   render() {
-    return <div>Here is some content explaining what students can do!</div>;
+    return (
+      <Container>
+        <div className="about">
+          <div className="title">
+            <h1>Students can help!</h1>
+          </div>
+          <div style={{ marginLeft: "15rem" }}>
+            <p>
+              You can participate in helping the building meet it's energy
+              goals.{" "}
+            </p>
+            <ul>
+              <li>Turn off lights when you leave the room.</li>
+              <li>Close any windows or doors that are letting heat out.</li>
+              <li>Be energy conscious in your behavior.</li>
+              <li>...</li>
+              <li>...</li>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    );
   }
 }
 
